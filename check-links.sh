e55e56b22d95ae7c22690644593966cd7e84c3ed
Python <lanemcunningham@gmail.com>
2018-12-31 15:11:40 -0600

    bash

--- check-links.sh
+++ check-links.sh
@@ -5,3 +5,7 @@ blc https://math.umn.edu -ro > results.txt & sleep 150 ; kill $!
 wait $!
 
 python parse_results.py
+
+wait $!
+
+echo "This is the message body" | mutt -a "results.txt" -s "test" -- lanemcunningham@gmail.com
