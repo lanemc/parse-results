f7f57021c1bba574967d7e8c4c60ad70e1cb5f0f
lane <lane@umn.edu>
2019-01-04 05:58:37 -0600

    moar commits

--- parse_results.py
+++ parse_results.py
@@ -2,7 +2,6 @@
 
 import csv
 import pandas as pd
-import re
 
 finalDict = {}
 
@@ -14,31 +13,21 @@ with open('results.txt', 'r') as f:
 	finalDict = dict((elem[0],elem[1:]) for elem in lines)
 	# Filter out identified failed redirects
 	finalDict = {k: [x for x in v if not any(item in x for item in ['├───OK───', 'https://math.umn.edu/about/memoriam', 'https://math.umn.edu/research/combinatorics', 'https://math.umn.edu/5309694', 'http://oit-drupal-prd-web.oit.umn.edu/', 'http://www.myu.umn.edu/', 'https://www.myu.umn.edu/', 'http://www.myu.umn.edu', 'https://math.umn.edu/NA', 'www.linkedin.com', 'http://www.fox.temple.edu/mcm_people/'])] for k, v in finalDict.items()}
+	# Replace '├─BROKEN─' with '' to clean the data
+	val = '├─BROKEN─'
+	for k, v in finalDict.items():
+		finalDict[k] = [x.replace(val, '').strip() for x in v]
 	# Remove dictionary keys (root links) that have no values
 	finalDict = {k:v for k,v in finalDict.items() if len(v) > 0}
 
-#for k in finalDict.keys():
-#	if k.contains('https://math.umn.edu/seminars/past-seminars-series/MCFAM-Seminar/MCIM-Industrial-Problems-Seminar/'):
-#		dic.pop(k)
-print(finalDict)
 
 # Create a DataFrame from finalDict using the dictionary keys as column headers and the Series method, which allows the transfer of the list of dictionary values into DataFrame rows.
 df = pd.DataFrame(dict([ (k,pd.Series(v)) for k,v in finalDict.iteritems() ]))
 
-#df = df.astype(str)
-#df.applymap(lambda x: re.sub('├─BROKEN─','',x))
-
-#print(df)
-# Declare the target and replacement values for NaT strings
-#findS = ['├───OK───']
-#replaceS = ['']
-
-# Replace NaT values with an empty string
-#def removeNonAscii(col):
-#        df[[col]] = df[[col]].replace(findS, replaceS)
-
-#removeNonAscii(df)
-
-#df.to_html('index.html')
+print(df)
 
 df.to_csv('new.csv', sep='\t', index=False)
+
+finalDf = pd.read_csv('new.csv', encoding='ascii')
+finalDf.to_html('index.html')
+
